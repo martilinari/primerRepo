f7061d52ac7585af9b3dff811c8d038741749bf3
martiniano linari <martinianolinari@gmail.com>
2020-06-28 18:40:51 +0200

    added some java code

---
+++ prueba/fileClass{1}.java
@@ -0,0 +1,17 @@
+pckage cosas;
+
+public class Lab {
+	public static void main(String[] args) {
+			System.out.println("Probando teclado nuevo.");
+					
+							
+								}
+								}ackage cosas;
+
+public class Lab {
+	public static void main(String[] args) {
+		System.out.println("Probando teclado nuevo.");
+		
+		
+	}
+}
